b3c5bd9d8b003c07d3bb36908396805a997b7c25
mshoaaaib <110221554+mshoaaaib@users.noreply.github.com>
2023-08-29 10:38:53 +0500

    Delete jira-config.js

--- jira-config.js
+++
@@ -1,5 +0,0 @@
-// Store your JIRA username (email) and API token here
-module.exports = {
-  jiraUsername: 'shoaib40ce@gmail.com',
-  jiraToken: 'ATATT3xFfGF0yptf0ZCu1pk-0LRwSe-dDTTF1TryfHU56z8d0fdWCs1hTzNaXToHPzBExQInKm4-AAfjkRQnqZG8bBxRg0JUUsfBeC4Y-1cGLRnRzEqlTKMB6BCndch0MwFSPgceK1GtJckDaZ4PJLUrj64i18tvZdVPS1v5euT0MB912HcwQLI=20E7168D'
-};
